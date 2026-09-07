c51a5ea18f70ad6dff174d50e79d3acbce4a437b
adamf0 <adamilkom00@gmail.com>
2026-09-07 12:09:20 +0700

    Use PhpOffice/PhpSpreadsheet for generating 100% valid Excel files

--- app/Services/SimpleXlsxWriter.php
+++ app/Services/SimpleXlsxWriter.php
@@ -2,10 +2,14 @@
 
 namespace App\Services;
 
+use PhpOffice\PhpSpreadsheet\Spreadsheet;
+use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
+use PhpOffice\PhpSpreadsheet\Style\Fill;
+
 class SimpleXlsxWriter
 {
     /**
-     * Create a 100% OpenXML-compliant .xlsx file at $outputPath
+     * Create an .xlsx file at $outputPath using PhpOffice\PhpSpreadsheet
      */
     public static function create($outputPath, array $headers, array $rows)
     {
@@ -18,157 +22,55 @@ class SimpleXlsxWriter
             @unlink($outputPath);
         }
 
-        $zip = new \ZipArchive();
-        if ($zip->open($outputPath, \ZipArchive::CREATE | \ZipArchive::OVERWRITE) !== true) {
-            return false;
-        }
+        $spreadsheet = new Spreadsheet();
+        $sheet = $spreadsheet->getActiveSheet();
+        $sheet->setTitle('Capaian IKU 1');
 
-        // Shared Strings collection
-        $stringMap = [];
-        $stringList = [];
-
-        $getStringIndex = function($str) use (&$stringMap, &$stringList) {
-            $str = (string)($str ?? '');
-            if (isset($stringMap[$str])) {
-                return $stringMap[$str];
-            }
-            $idx = count($stringList);
-            $stringMap[$str] = $idx;
-            $stringList[] = $str;
-            return $idx;
-        };
-
-        // 1. [Content_Types].xml
-        $contentTypes = '<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
-<Types xmlns="http://schemas.openxmlformats.org/package/2006/content-types">
-  <Default Extension="rels" ContentType="application/vnd.openxmlformats-package.relationships+xml"/>
-  <Default Extension="xml" ContentType="application/xml"/>
-  <Override PartName="/xl/workbook.xml" ContentType="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet.main+xml"/>
-  <Override PartName="/xl/worksheets/sheet1.xml" ContentType="application/vnd.openxmlformats-officedocument.spreadsheetml.worksheet+xml"/>
-  <Override PartName="/xl/styles.xml" ContentType="application/vnd.openxmlformats-officedocument.spreadsheetml.styles+xml"/>
-  <Override PartName="/xl/sharedStrings.xml" ContentType="application/vnd.openxmlformats-officedocument.spreadsheetml.sharedStrings+xml"/>
-</Types>';
-        $zip->addFromString('[Content_Types].xml', $contentTypes);
-
-        // 2. _rels/.rels
-        $rels = '<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
-<Relationships xmlns="http://schemas.openxmlformats.org/package/2006/relationships">
-  <Relationship Id="rId1" Type="http://schemas.openxmlformats.org/officeDocument/2006/relationships/officeDocument" Target="xl/workbook.xml"/>
-</Relationships>';
-        $zip->addFromString('_rels/.rels', $rels);
-
-        // 3. xl/_rels/workbook.xml.rels
-        $wbRels = '<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
-<Relationships xmlns="http://schemas.openxmlformats.org/officeDocument/2006/relationships">
-  <Relationship Id="rId1" Type="http://schemas.openxmlformats.org/officeDocument/2006/relationships/worksheet" Target="worksheets/sheet1.xml"/>
-  <Relationship Id="rId2" Type="http://schemas.openxmlformats.org/officeDocument/2006/relationships/styles" Target="styles.xml"/>
-  <Relationship Id="rId3" Type="http://schemas.openxmlformats.org/officeDocument/2006/relationships/sharedStrings" Target="sharedStrings.xml"/>
-</Relationships>';
-        $zip->addFromString('xl/_rels/workbook.xml.rels', $wbRels);
-
-        // 4. xl/workbook.xml
-        $workbook = '<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
-<workbook xmlns="http://schemas.openxmlformats.org/spreadsheetml/2006/main" xmlns:r="http://schemas.openxmlformats.org/officeDocument/2006/relationships">
-  <fileVersion appName="xl" lastEdited="5" lowestEdited="5" rupBuild="9303"/>
-  <workbookPr defaultThemeVersion="124226"/>
-  <bookViews>
-    <workbookView xWindow="0" yWindow="0" windowWidth="15000" windowHeight="10000"/>
-  </bookViews>
-  <sheets>
-    <sheet name="Sheet1" sheetId="1" r:id="rId1"/>
-  </sheets>
-  <calcPr calcId="124519"/>
-</workbook>';
-        $zip->addFromString('xl/workbook.xml', $workbook);
-
-        // 5. xl/styles.xml
-        $styles = '<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
-<styleSheet xmlns="http://schemas.openxmlformats.org/spreadsheetml/2006/main">
-  <fonts count="1">
-    <font><sz val="11"/><name val="Calibri"/></font>
-  </fonts>
-  <fills count="2">
-    <fill><patternFill patternType="none"/></fill>
-    <fill><patternFill patternType="gray125"/></fill>
-  </fills>
-  <borders count="1">
-    <border><left/><right/><top/><bottom/></border>
-  </borders>
-  <cellStyleXfs count="1">
-    <xf numFmtId="0" fontId="0" fillId="0" borderId="0"/>
-  </cellStyleXfs>
-  <cellXfs count="1">
-    <xf numFmtId="0" fontId="0" fillId="0" borderId="0" xfId="0"/>
-  </cellXfs>
-</styleSheet>';
-        $zip->addFromString('xl/styles.xml', $styles);
-
-        // 6. Build xl/worksheets/sheet1.xml
-        $sheetData = '<sheetData>';
         $rowIdx = 1;
 
         // Header Row
         if (!empty($headers)) {
-            $sheetData .= '<row r="1">';
-            $colIdx = 0;
+            $colIdx = 1;
             foreach ($headers as $h) {
-                $cellRef = self::colName($colIdx) . '1';
-                $sIdx = $getStringIndex((string)$h);
-                $sheetData .= '<c r="' . $cellRef . '" t="s"><v>' . $sIdx . '</v></c>';
+                $cell = $sheet->getCellByColumnAndRow($colIdx, 1);
+                $cell->setValue((string)$h);
                 $colIdx++;
             }
-            $sheetData .= '</row>';
+
+            $highestCol = $sheet->getHighestColumn();
+            $headerRange = "A1:{$highestCol}1";
+            $sheet->getStyle($headerRange)->getFont()->setBold(true);
+            $sheet->getStyle($headerRange)->getFill()
+                ->setFillType(Fill::FILL_SOLID)
+                ->getStartColor()->setARGB('EAEAEA');
+
             $rowIdx++;
         }
 
         // Data Rows
         foreach ($rows as $row) {
-            $sheetData .= '<row r="' . $rowIdx . '">';
-            $colIdx = 0;
+            $colIdx = 1;
             foreach ($row as $val) {
-                $cellRef = self::colName($colIdx) . $rowIdx;
+                $cell = $sheet->getCellByColumnAndRow($colIdx, $rowIdx);
                 if (is_numeric($val) && !preg_match('/^0\d+/', (string)$val)) {
-                    $sheetData .= '<c r="' . $cellRef . '"><v>' . $val . '</v></c>';
+                    $cell->setValue((float)$val);
                 } else {
-                    $sIdx = $getStringIndex((string)($val ?? ''));
-                    $sheetData .= '<c r="' . $cellRef . '" t="s"><v>' . $sIdx . '</v></c>';
+                    $cell->setValue((string)($val ?? ''));
                 }
                 $colIdx++;
             }
-            $sheetData .= '</row>';
             $rowIdx++;
         }
 
-        $sheetData .= '</sheetData>';
-
-        $sheet1 = '<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
-<worksheet xmlns="http://schemas.openxmlformats.org/spreadsheetml/2006/main">
-' . $sheetData . '
-</worksheet>';
-        $zip->addFromString('xl/worksheets/sheet1.xml', $sheet1);
-
-        // 7. Build xl/sharedStrings.xml
-        $sstXml = '<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
-<sst xmlns="http://schemas.openxmlformats.org/spreadsheetml/2006/main" count="' . count($stringList) . '" uniqueCount="' . count($stringList) . '">';
-        foreach ($stringList as $str) {
-            $safeStr = htmlspecialchars($str, ENT_QUOTES | ENT_XML1, 'UTF-8');
-            $sstXml .= '<si><t xml:space="preserve">' . $safeStr . '</t></si>';
+        // Auto-fit column widths
+        $maxCols = !empty($headers) ? count($headers) : 6;
+        for ($col = 1; $col <= $maxCols; $col++) {
+            $sheet->getColumnDimensionByColumn($col)->setAutoSize(true);
         }
-        $sstXml .= '</sst>';
-        $zip->addFromString('xl/sharedStrings.xml', $sstXml);
 
-        $zip->close();
+        $writer = new Xlsx($spreadsheet);
+        $writer->save($outputPath);
 
         return file_exists($outputPath);
-    }
-
-    private static function colName($index)
-    {
-        $letters = '';
-        while ($index >= 0) {
-            $letters = chr($index % 26 + 65) . $letters;
-            $index = intval($index / 26) - 1;
-        }
-        return $letters;
     }
 }
